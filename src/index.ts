481c74b5ded794246882f48307244e788384db37
Maycon <mayconsubrim2002@gmail.com>
2024-06-20 05:21:44 -0300

    atualiza configuração do cors

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,16 @@ import calcRouter from './routers/calcRouter';
 const app = express();
 const port = 3000;
 
-app.use(cors({
+
+const corsOptions = {
   origin: 'http://localhost:4200', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
-}));
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use('/calc', calcRouter);
 
